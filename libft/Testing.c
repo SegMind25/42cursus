d825f712313dd2947fee38a9712870b21b626b00
SegMind25 <modistotube2004@gmail.com>
2026-02-10 11:52:10 +0100

    The Testing Ways

---
+++ libft/Testing.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+int ft_strlen(char *str)
+{
+  int i = 0;
+  while(str[i])
+    i++;
+  return i;
+}
+
+int main()
+{
+  char str[] = "Bekkali";
+
+  printf("%i\n", ft_strlen(str));
+}
